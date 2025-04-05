5903cc47666a685c230df9704c507d3fa3f05639
Lassi Kuisma <lassi.kuisma1@gmail.com>
2025-04-05 13:34:09 +0300

    harpoon projectile

---
+++ characters/harpoon_projectile.gd
@@ -0,0 +1,12 @@
+class_name HarpoonProjectile
+extends Area2D
+
+var speed = 100.0
+
+func _physics_process(delta: float) -> void:
+	position.x += speed * delta
+	# position += transform.x * speed * delta
+
+func _on_body_entered(other: Node2D) -> void:
+	print("harpoon hit something...")
+	self.queue_free()
--- characters/player.gd
+++ characters/player.gd
@@ -9,6 +9,10 @@ var hp = max_hp
 
 var is_in_transition: bool = false
 
+var harpoon_scene = preload("res://characters/harpoon_projectile.tscn")
+var is_harpoon_ready = true
+@export var harpoon_cooldown = 0.75
+
 func _physics_process(delta: float) -> void:
 	if is_in_transition:
 		return
@@ -27,7 +31,23 @@ func _physics_process(delta: float) -> void:
 
 	move_and_slide()
 
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("attack_1"):
+		do_harpoon_attack()
+
 func take_damage(amount: int):
 	hp -= amount
 	print("Ouch, my hp is: ", hp)
 	$AnimationPlayer.play("take_damage")
+
+func do_harpoon_attack():
+	if !is_harpoon_ready:
+		return
+	
+	var projectile: HarpoonProjectile = harpoon_scene.instantiate()
+	projectile.global_position = $HarpoonSpawn.global_position
+	get_parent().add_child(projectile)
+	
+	is_harpoon_ready = false
+	await get_tree().create_timer(harpoon_cooldown).timeout
+	is_harpoon_ready = true
