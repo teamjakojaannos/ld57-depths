f6271fa69ca9ebdaadb28f869c2fc1b038cf8aae
Lassi Kuisma <lassi.kuisma1@gmail.com>
2025-04-05 14:49:27 +0300

    spawn enemies

--- levels/level.gd
+++ levels/level.gd
@@ -3,5 +3,20 @@ class_name Level
 
 signal Finished
 
+var spike_fish_prefab = preload("res://characters/spike_fish.tscn")
+
+func _ready() -> void:
+	spawn_enemies()
+
 func finish() -> void:
 	Finished.emit()
+
+func spawn_enemies():
+	var spawn_points = $SpikeFishSpawns.get_children()
+	if spawn_points.size() == 0:
+		return
+	var spawn_point = spawn_points.pick_random()
+
+	var fish: Node2D = spike_fish_prefab.instantiate()
+	fish.global_position = spawn_point.global_position
+	add_child(fish)
