2d2214c88e2ab974fde40bd4d41b6280c846692f
Lassi Kuisma <lassi.kuisma1@gmail.com>
2025-04-06 21:09:56 +0300

    lion fish cleanup

--- fish/leijjjonakala/lion_fish.gd
+++ fish/leijjjonakala/lion_fish.gd
@@ -5,6 +5,7 @@ extends Area2D
 @export var after_charge_speed = 100.0
 var speed = 0.0
 
+var braking_tween: Tween
 var velocity = Vector2()
 
 enum State {
@@ -14,16 +15,7 @@ enum State {
 }
 
 var current_state = State.Idle
-var charge_target = Vector2()
-# little explanation here: when the fish reaches the charge target, we save
-# the velocity it had to this variable. Then it starts moving towards some
-# random spot (the well named "after charge target"). However, in order to avoid
-# sharp turns, we lerp the fish's velocity between "velocity fish had when it charged"
-# and "new velocity that takes fish to the new target" with "velocity weight" which
-# is simply tweened from 0->1 over some duration
-# In other words, acceleration...
-var final_charge_velocity = Vector2()
-var after_charge_target = Vector2()
+var target = Vector2()
 var velocity_weight = 0.0
 
 func _ready() -> void:
@@ -31,19 +23,22 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	var close_enough = 100.0
+	if $Health.is_dead:
+		position += velocity
+		return
 	
 	if current_state == State.Charge:
-		velocity = (charge_target - position).normalized() * delta * speed
+		velocity = (target - position).normalized() * delta * speed
 
-		if position.distance_squared_to(charge_target) <= close_enough:
-			run_away_after_charge(velocity)
+		if position.distance_squared_to(target) <= close_enough:
+			run_away_after_charge()
 			return
 	
 	if current_state == State.AfterCharge:
-		var new_velocity = (after_charge_target - position).normalized() * delta * speed
-		velocity = final_charge_velocity.lerp(new_velocity, velocity_weight)
+		var new_velocity = (target - position).normalized() * delta * speed
+		velocity = velocity.lerp(new_velocity, velocity_weight)
 		
-		if position.distance_squared_to(after_charge_target) <= close_enough:
+		if position.distance_squared_to(target) <= close_enough:
 			start_idling()
 			return
 	
@@ -52,17 +47,22 @@ func _physics_process(delta: float) -> void:
 
 func start_charging():
 	current_state = State.Charge
-	charge_target = Globals.player.position
+	target = Globals.player.position
 	var tween = create_tween()
 	var acceleration_time = 1.0
+	speed = 0.0
 	tween.tween_property(self, "speed", charge_speed, acceleration_time)
 
-func run_away_after_charge(charge_velocity: Vector2):
+func run_away_after_charge():
 	current_state = State.AfterCharge
 	speed = after_charge_speed
-	after_charge_target = Globals.current_level.get_random_fish_nav_point()
-	final_charge_velocity = charge_velocity
+	target = Globals.current_level.get_random_fish_nav_point()
 
+	# a bit of explanation here:
+	# when fish reaches the charge target, it picks a random point and starts
+	# moving towards that. However, to avoid sharp turns and make the movement
+	# more fluid, we lerp between "go to current target"-velocity and "velocity
+	# I had when I was charging", using this variable as weight
 	velocity_weight = 0.0
 	var tween = create_tween()
 	var deceleration_time = 1.5
@@ -70,15 +70,22 @@ func run_away_after_charge(charge_velocity: Vector2):
 
 func start_idling():
 	current_state = State.Idle
-	var tween = create_tween()
+	speed = 0.0
+
+	if braking_tween != null:
+		braking_tween.kill()
+	braking_tween = create_tween()
 	var deceleration_time = 1.0
-	tween.set_ease(Tween.EASE_OUT)
-	tween.tween_property(self, "velocity", Vector2(0, 0), deceleration_time)
+	braking_tween.set_ease(Tween.EASE_OUT)
+	braking_tween.tween_property(self, "velocity", Vector2(0, 0), deceleration_time)
 	
 	var idle_time = randf_range(1.0, 4.0)
 	await get_tree().create_timer(idle_time).timeout
+	braking_tween.kill()
 	
-	tween.kill()
+	if $Health.is_dead:
+		return
+	
 	start_charging()
 
 func _on_area_entered(other: Area2D) -> void:
@@ -94,7 +101,10 @@ func _on_health_die() -> void:
 	Globals.level.current_level.record_kill()
 
 	$HurtAnimations.play("die")
+	if braking_tween != null:
+		braking_tween.kill()
+
 	var tween = create_tween()
-	tween.tween_property(self, "speed", 0.0, 1.0)
+	tween.tween_property(self, "velocity", Vector2(0, 0), 1.0)
 	await $HurtAnimations.animation_finished
 	queue_free()
