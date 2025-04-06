e63a3de4ad6bb3632ee83e52195572ecc6a5d6f8
Lassi Kuisma <lassi.kuisma1@gmail.com>
2025-04-06 22:26:21 +0300

    i-frames

--- characters/health.gd
+++ characters/health.gd
@@ -11,6 +11,9 @@ signal hurt_at(position: Vector2)
 
 @export var max_health: float = 10
 
+@export var invincible_time: float = 0.1
+var _is_invincible = false
+
 var is_dead: bool:
 	get:
 		return _health <= 0
@@ -24,15 +27,23 @@ func heal(amount: float, _from: Node) -> void:
 	_check_signals(old_health, Vector2.INF)
 
 func take_damage(amount: float, _from: Node) -> void:
-	var old_health = _health
-	_health = clamp(_health - amount, 0, max_health)
-	_check_signals(old_health, Vector2.INF)
+	_try_take_damage(amount, _from, Vector2.INF)
 
 func take_damage_at(amount: float, _from: Node, point: Vector2) -> void:
+	_try_take_damage(amount, _from, point)
+
+func _try_take_damage(amount: float, _from: Node, point: Vector2) -> void:
+	if _is_invincible:
+		return
+	
 	var old_health = _health
 	_health = clamp(_health - amount, 0, max_health)
 	_check_signals(old_health, point)
-
+	
+	if invincible_time > 0:
+		_is_invincible = true
+		await get_tree().create_timer(invincible_time).timeout
+		_is_invincible = false
 
 func _ready() -> void:
 	_health = max_health
