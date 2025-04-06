689baae080beb3962a3e76a0de2e38393e42c046
Lassi Kuisma <lassi.kuisma1@gmail.com>
2025-04-06 23:34:30 +0300

    anchor sfx

---
+++ characters/anchor_impact.gd
@@ -0,0 +1,24 @@
+extends Node
+
+# TODO: replace with proper sounds!
+const mob_impact: AudioStream = preload("uid://dvd74jspqa7l5")
+const wall_impact: AudioStream = preload("uid://dwg51c0vur3xw")
+
+func _on_hurtbox_hurt_target(other: Hitbox) -> void:
+	var stream = AudioStreamPlayer.new()
+	stream.stream = mob_impact
+	stream.volume_db = -3.0
+	stream.bus = "Player Guns SFX"
+
+	Globals.current_level.add_child(stream)
+	stream.play()
+
+
+func _on_anchor_body_entered(body: Node2D) -> void:
+	var stream = AudioStreamPlayer.new()
+	stream.stream = wall_impact
+	stream.volume_db = -2.5
+	stream.bus = "Player Guns SFX"
+
+	Globals.current_level.add_child(stream)
+	stream.play()
---
+++ characters/sfx_anchor.gd
@@ -0,0 +1,4 @@
+extends AudioStreamPlayer
+
+func _on_anchor_dropper_fire(shooter: CharacterBody2D, direction: Vector2) -> void:
+	play()
