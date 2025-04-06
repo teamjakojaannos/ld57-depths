932f18c4e3d34d0360fd493826415755351add8a
Lassi Kuisma <lassi.kuisma1@gmail.com>
2025-04-06 13:23:23 +0300

    move fish movement tweaks

--- fish/spiky_puffer_fish/spike_fish.gd
+++ fish/spiky_puffer_fish/spike_fish.gd
@@ -60,15 +60,16 @@ func _create_movement_tweens(force_recreate: bool = false):
 func _physics_process(delta: float) -> void:
 	var target_position = (target1 + target2) / 2.0
 	var previous_position = position
+	
+	var d2 = position.distance_squared_to(target_position)
+	var close_enough = 500.0
+	if d2 <= close_enough:
+		return
+	
 	position = position.move_toward(target_position, move_speed * delta)
 	
 	var movement = position - previous_position
 	$AnimatedSprite2D.flip_h = movement.x > 0
-	
-	var d2 = position.distance_squared_to(target_position)
-	var close_enough = 10.0
-	if d2 <= close_enough:
-		_create_movement_tweens(true)
 
 	if ready_to_attack and !is_dead:
 		attack()
