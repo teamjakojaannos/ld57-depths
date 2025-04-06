4d2b07d040b2348e22b7703dd6cea43dcd2643c2
Lassi Kuisma <lassi.kuisma1@gmail.com>
2025-04-06 23:19:36 +0300

    anchor projectile

--- characters/anchor_dropper.gd
+++ characters/anchor_dropper.gd
@@ -1,4 +1,17 @@
 extends Marker2D
 
+const anchor_prefab = preload("res://characters/anchor.tscn")
+
+@export var anchor_damage = 5
+@export var anchor_fall_speed = -400.0
+
 func _on_anchor_dropper_fire(shooter: CharacterBody2D, direction: Vector2) -> void:
-	print("bonk goes the anchor")
+	var anchor: Projectile = anchor_prefab.instantiate()
+	anchor.global_position = global_position
+	anchor.velocity = Vector2(0, 0)
+	anchor.fall_speed = anchor_fall_speed
+	anchor.damage = anchor_damage
+	# TODO: inherit downwards momentum?
+	
+	var current_map = Globals.current_level
+	current_map.add_child(anchor)
