5a5aa2d1ce88633c1fab202002f8a7f6f09f99a3
Lassi Kuisma <lassi.kuisma1@gmail.com>
2025-04-07 15:33:42 +0300

    money

--- ui/money_counter.gd
+++ ui/money_counter.gd
@@ -1,8 +1,21 @@
 extends HBoxContainer
 
+var money_tween: Tween
+var current_value = 0
+
 func _ready() -> void:
 	Globals.money_changed.connect(update_label)
-	update_label()
+	set_label_text(Globals.money)
 
 func update_label():
-	$Label.text = "%s x" % Globals.money
+	if money_tween != null:
+		money_tween.kill()
+	
+	money_tween = create_tween()
+	var time = 1.0
+	money_tween.tween_method(set_label_text, current_value, Globals.money, time)
+
+
+func set_label_text(value: int):
+	current_value = value
+	$Label.text = "%s x" % current_value
