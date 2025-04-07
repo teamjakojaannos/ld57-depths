ed8e47f34464148f5d7f886ab062eaa4137c77ba
Lassi Kuisma <lassi.kuisma1@gmail.com>
2025-04-07 13:39:57 +0300

    music fade when switching tracks

--- levels/music.gd
+++ levels/music.gd
@@ -10,6 +10,10 @@ const music_track_1: AudioStream = preload("res://levels/bgm1.ogg")
 const music_track_2: AudioStream = preload("res://levels/bgm2.ogg")
 const boss_music_1: AudioStream = preload("res://levels/bgm3.ogg")
 
+var music_fade_tween: Tween
+var music_fade_mult = 1.0
+var counter = 0
+
 func _ready() -> void:
 	Globals.volume_changed.connect(update_volume)
 	
@@ -18,18 +22,45 @@ func _ready() -> void:
 	play()
 
 func update_volume():
-	self.volume_linear = Globals.music_volume_percent
+	self.volume_linear = Globals.music_volume_percent * music_fade_mult
 
 func play_song(song: Song):
+	var song_to_play
 	if song == Song.Music_1:
-		stream = music_track_1
-		play()
+		song_to_play = music_track_1
 	elif song == Song.Music_2:
-		stream = music_track_2
-		play()
+		song_to_play = music_track_2
 	elif song == Song.Boss_1:
-		stream = boss_music_1
-		play()
+		song_to_play = boss_music_1
+	else:
+		print("Trying to play song that doesn't exist: ", song)
+		return
+	
+	
+	if music_fade_tween != null:
+		music_fade_tween.kill()
+	
+	counter += 1
+	var current_id = counter
+	
+	music_fade_tween = create_tween()
+	var fade_time = 1.0
+	await music_fade_tween.tween_method(do_music_fade, music_fade_mult, 0.0, fade_time).finished
+	
+	# if we switched to another song while fading out, cancel the song-switch
+	if current_id != counter:
+		return
+	
+	stream = song_to_play
+	play()
+	
+	music_fade_tween = create_tween()
+	var un_fade_time = 1.0
+	await music_fade_tween.tween_method(do_music_fade, music_fade_mult, 1.0, un_fade_time).finished
+
+func do_music_fade(fade_mult: float):
+	music_fade_mult = fade_mult
+	update_volume()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("switch_track_1"):
