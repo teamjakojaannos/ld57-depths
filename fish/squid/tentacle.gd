f4cdeb239c02796b28db24eb8b4a1549ddb5c15f
Lassi Kuisma <lassi.kuisma1@gmail.com>
2025-04-07 20:41:57 +0300

    tentacle sequence attack

--- fish/squid/tentacle.gd
+++ fish/squid/tentacle.gd
@@ -10,11 +10,15 @@ var max_speed: float
 var start_pos: Vector2
 var end_pos: Vector2
 var wait_time_before_attack: float = 1.0
+var reverse_after_fully_extended = true
 var _going_forward = true
 var _can_move = false
 var _speed: float
+var signal_emitted_fully_extended = false
+var signal_emitted_attack_done = false
 
 signal attack_done
+signal fully_extended
 
 func _ready() -> void:
 	$AnimationPlayer.play("emerge")
@@ -40,12 +44,18 @@ func _physics_process(delta: float) -> void:
 	
 	if position.distance_squared_to(target) <= close_enough:
 		if _going_forward:
-			_going_forward = false
+			if !signal_emitted_fully_extended:
+				signal_emitted_fully_extended = true
+				fully_extended.emit()
+			if reverse_after_fully_extended:
+				_going_forward = false
 		else:
 			_attack_done()
 
 func _attack_done():
-	attack_done.emit()
+	if !signal_emitted_attack_done:
+		signal_emitted_attack_done = true
+		attack_done.emit()
 	$AnimationPlayer.play_backwards("emerge")
 	await $AnimationPlayer.animation_finished
 	queue_free()
--- levels/generation/templates/special/squid_fight/squid_fight_level.gd
+++ levels/generation/templates/special/squid_fight/squid_fight_level.gd
@@ -3,3 +3,10 @@ extends Node2D
 
 func get_random_tentacle_spawn() -> TentacleMarkers:
 	return $TentacleSpawns.get_children().pick_random()
+
+func get_tentacle_sequence_positions() -> Array[TentacleMarkers]:
+	var result: Array[TentacleMarkers]
+	result.append($TentacleSequence/First)
+	result.append($TentacleSequence/Second)
+	result.append($TentacleSequence/Third)
+	return result
