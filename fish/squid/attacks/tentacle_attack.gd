ce3cb97c82d9685be2a46b120f1867cd66e40444
Lassi Kuisma <lassi.kuisma1@gmail.com>
2025-04-07 19:37:30 +0300

    tentacle emerge animation

--- fish/squid/attacks/tentacle_attack.gd
+++ fish/squid/attacks/tentacle_attack.gd
@@ -2,24 +2,29 @@ extends SquidAttack
 
 const tentacle_prefab: PackedScene = preload("res://fish/squid/tentacle.tscn")
 
-@export var tentacle_speed = 100.0
+@export var wait_time_before_attack = 0.5
+@export var tentacle_speed = 400.0
 @export var tentacle_damage = 1
 
 func do_attack(level: SquidFightLevel):
 	var positions = level.get_random_tentacle_spawn()
 	var tentacle: Tentacle = tentacle_prefab.instantiate()
 	
-	tentacle.damage = tentacle_damage
-	tentacle.speed = tentacle_speed
 	
 	# get pos relative to level
 	var spawn_position = positions.spawn.position + positions.position
 	var end_position = positions.end.position + positions.position
 	
+	tentacle.damage = tentacle_damage
+	tentacle.max_speed = tentacle_speed
+	tentacle.initial_speed = tentacle_speed / 4.0
+	tentacle.speed_up_time = 0.5
+	tentacle.wait_time_before_attack = wait_time_before_attack
 	tentacle.position = spawn_position
 	tentacle.start_pos = spawn_position
 	tentacle.end_pos = end_position
 	tentacle.set_flipped(!positions.is_left_to_right)
+	tentacle.visible = false
 	
 	level.add_child(tentacle)
 	
--- fish/squid/tentacle.gd
+++ fish/squid/tentacle.gd
@@ -4,22 +4,39 @@ extends Node2D
 var damage: float:
 	set(value):
 		$Hurtbox.damage = value
-var speed: float
+var speed_up_time: float = 1.0
+var initial_speed: float
+var max_speed: float
 var start_pos: Vector2
 var end_pos: Vector2
+var wait_time_before_attack: float = 1.0
 var _going_forward = true
+var _can_move = false
+var _speed: float
 
 signal attack_done
+
+func _ready() -> void:
+	$AnimationPlayer.play("emerge")
+	await $AnimationPlayer.animation_finished
+	await get_tree().create_timer(wait_time_before_attack).timeout
+	_can_move = true
+	_speed = initial_speed
+	var speed_tween = create_tween()
+	speed_tween.tween_property(self, "_speed", max_speed, speed_up_time)
 
 func set_flipped(flipped: bool):
 	scale = Vector2(-1, 1) if flipped else Vector2(1, 1)
 
 func _physics_process(delta: float) -> void:
+	if !_can_move:
+		return
+	
 	var close_enough = 20.0
 	
 	var target = end_pos if _going_forward else start_pos
 	
-	position = position.move_toward(target, delta * speed)
+	position = position.move_toward(target, delta * _speed)
 	
 	if position.distance_squared_to(target) <= close_enough:
 		if _going_forward:
@@ -29,4 +46,6 @@ func _physics_process(delta: float) -> void:
 
 func _attack_done():
 	attack_done.emit()
+	$AnimationPlayer.play_backwards("emerge")
+	await $AnimationPlayer.animation_finished
 	queue_free()
