50c03b3f8be9884514cca3cbad2e3804f94b8743
Inari Tommiska <inari@jakojaannos.fi>
2025-04-07 18:33:23 +0300

    fix level counter

--- levels/level_indicator.gd
+++ levels/level_indicator.gd
@@ -1,4 +1,11 @@
 extends Label
 
 func _process(_delta: float) -> void:
-	text = "Level: %s" % Globals.current_room_index
+	var tutorial_room_count = 2
+	var current = Globals.current_room_index
+	if current <= tutorial_room_count:
+		text = "Tutorial level %s" % (current + 1)
+	else:
+		var idx = current - tutorial_room_count
+		text = "Level: %s" % idx
+		
