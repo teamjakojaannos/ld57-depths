fdc272f36587a9ab9c4f8a5a26d90e182df55475
Lassi Kuisma <lassi.kuisma1@gmail.com>
2025-04-07 11:21:12 +0300

    crab movement

--- fish/little_crab/little_crab.gd
+++ fish/little_crab/little_crab.gd
@@ -1,10 +1,12 @@
 extends PathFollow2D
 
 @export var move_speed = 10.0
+var move_direction: int = 1
 
 func _ready() -> void:
-	pass
-	#progress_ratio = randf()
+	progress_ratio = randf()
+	var moves_forward = randi_range(0, 1) == 0
+	move_direction = 1 if moves_forward else -1
 
 func _physics_process(delta: float) -> void:
-	progress += move_speed * delta
+	progress += move_speed * move_direction * delta
