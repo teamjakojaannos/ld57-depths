f60a5c6920528ca3f03b6e510b3f3f79b8b7bf4b
Inari Tommiska <inari@jakojaannos.fi>
2025-04-08 04:14:44 +0300

    fixfixifx

--- characters/player/harpoongun.gd
+++ characters/player/harpoongun.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name HarpoonGun
 
 const harpoon_scene = preload("res://characters/harpoon_projectile.tscn")
+const harpoon_scene_better = preload("res://characters/harpoon_projectile_but_better.tscn")
 @export var projectile_speed: float = 500.0
 @export var projectile_damage: float = 1.0
 
@@ -9,7 +10,11 @@ func _on_harpoon_gun_fire(shooter: CharacterBody2D, direction: Vector2) -> void:
 	_spawn_projectile(shooter, direction)
 
 func _spawn_projectile(_shooter: CharacterBody2D, direction: Vector2) -> void:
-	var projectile: Projectile = harpoon_scene.instantiate()
+	var prefab = harpoon_scene
+	if projectile_damage >= 3.0:
+		prefab = harpoon_scene_better
+
+	var projectile: Projectile = prefab.instantiate()
 
 	var offset = position
 	projectile.global_position = get_parent().global_position + offset
