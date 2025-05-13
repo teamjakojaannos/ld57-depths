4eb42972aac21c77424ab5e345e02df8ec68779c
Inari Tommiska <inari@jakojaannos.fi>
2025-05-13 20:30:18 +0300

    drive player animations with animationtree
    Attempt at decoupling animations from the player movement logic. This
    should/could make debugging the animations easier. Hopefully.
    
    The tools of the trade here is an AnimationTree. The tree uses a bunch
    of nested state machines to make all possible permutations of different
    states smoothly transition to one another. Most of the nested machines
    are used just for handling player facing left/right, while the root
    level state machine handles moving between the various states.
    
    Some states (e.g. dying/jumping) are not something player is
    continuously in, but rather happen once, with player either stuck in
    them (dead) or smoothly transferring to other states (jump -> fall).
    These are easily modeled using the animation tree, but the initial
    transition is a bit tricky.
    
    The jump transition for example needs to be one-off, so having a
    continuously truthy condition/expression for it is a bit iffy. Likewise,
    death is easiest to detect with the "player died" -signal, instead of
    some variable/expression.
    
    Therefore, I ended up making those transitions as "state
    teleportations", meaning that there is no transition for these in the
    state machine, but rather a signal handler teleports/travels to those
    states via code. This is a tradeoff between now having two different
    kinds of transitions for the animations and the ease of implementation.
    
    To make matters a bit less confusing, I named these special states with
    action_* -prefix, to indicate they may be teleported to.
    
    All-in-all, the animation state machine can now be debugged in-editor,
    the animations are driven with a animation tree with two-layer
    underlying animation player thingamajig and there is still a script for
    defining the various conditions for the animation. Therefore, the
    complexity didn't go down, but rather a notch up, which is somewhat
    counterintuitive.
    
    However, I argue that the script is now _much_ simpler, and the visual
    representation of the AnimationTree is much easier to grasp than the
    spaghetti the old 'player_animations.gd' was.
    
    There's likely still a lot to improve, but learning to use the tools
    takes time, I guess :)

---
+++ characters/player/player_sprite.gd
@@ -0,0 +1,79 @@
+@tool
+extends Node2D
+
+@onready var player: Player = $".."
+@onready var animation_tree: AnimationTree = $AnimationTree
+
+var animation_root: AnimationNodeStateMachinePlayback:
+	get:
+		return animation_tree["parameters/playback"]
+
+enum Facing {
+	LEFT,
+	RIGHT,
+}
+
+@export var debug_facing: Facing = Facing.RIGHT
+@export var debug_is_walking: bool = false
+@export var debug_is_falling: bool = false
+@export var debug_is_crouching: bool = false
+
+@export_tool_button("Jump")
+var debug_player_jump: Callable = _on_player_jumped
+
+@export_tool_button("Die")
+var debug_player_die: Callable = _on_player_die
+
+
+var is_walking: bool:
+	get:
+		if is_falling or is_crouching:
+			return false
+
+		if Engine.is_editor_hint():
+			return debug_is_walking
+
+		return player.is_moving
+
+var is_crouching: bool:
+	get:
+		if is_falling:
+			return false
+
+		if Engine.is_editor_hint():
+			return debug_is_crouching
+
+		return player.is_crouching
+
+var is_falling: bool:
+	get:
+		if Engine.is_editor_hint():
+			return debug_is_falling
+
+		return !player.is_on_floor()
+
+var facing: Facing:
+	get:
+		if Engine.is_editor_hint():
+			return debug_facing
+
+		if player.looking_at == Player.LookingAt.LEFT:
+			return Facing.LEFT
+		else:
+			return Facing.RIGHT
+
+
+func _on_player_jumped() -> void:
+	animation_root.travel("action_jump")
+
+func _on_player_hurt() -> void:
+	var blinker = create_tween()
+	blinker.set_trans(Tween.TRANS_EXPO)
+
+	var blink_duration = 0.1
+	blinker.tween_property(player, "modulate", Color.ORANGE_RED, blink_duration * 0.5)
+	blinker.tween_property(player, "modulate", Color.WHITE, blink_duration * 0.5)
+	blinker.set_loops(10)
+
+func _on_player_die() -> void:
+	animation_root.travel("action_die")
