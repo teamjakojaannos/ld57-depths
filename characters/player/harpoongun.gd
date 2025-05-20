109c73a3e7c9d8fe75ad925876ddb9f5811e14f7
Inari Tommiska <inari@jakojaannos.fi>
2025-05-20 16:23:27 +0300

    decouple shooting offset from player animations
    To clean up the player scene, it'd be nice to extract the player visuals
    to their own scene. However, the animations are currently still strongly
    coupled with the physics at some places, making this
    difficult/impossible.
    
    Likely the last piece of this puzzle is decoupling the harpoon spawner
    position from the player anims. After this, the player_sprite.gd and the
    animations it is driving should now be fully self-contained, with just
    signals coming in from the parent player.gd.

--- characters/player/harpoongun.gd
+++ characters/player/harpoongun.gd
@@ -6,10 +6,12 @@ const harpoon_scene_better = preload("res://characters/harpoon_projectile_but_be
 @export var projectile_speed: float = 500.0
 @export var projectile_damage: float = 1.0
 
+@export var crouch_offset: float = 9.0
+
 func _on_harpoon_gun_fire(shooter: CharacterBody2D, direction: Vector2) -> void:
 	_spawn_projectile(shooter, direction)
 
-func _spawn_projectile(_shooter: CharacterBody2D, direction: Vector2) -> void:
+func _spawn_projectile(shooter: CharacterBody2D, direction: Vector2) -> void:
 	var prefab = harpoon_scene
 	if projectile_damage >= 3.0:
 		prefab = harpoon_scene_better
@@ -17,9 +19,14 @@ func _spawn_projectile(_shooter: CharacterBody2D, direction: Vector2) -> void:
 	var projectile: Projectile = prefab.instantiate()
 
 	var offset = position
+	offset.x *= sign(direction.x)
+	if shooter is Player:
+		if shooter.is_crouching:
+			offset += Vector2.DOWN * crouch_offset
+
 	projectile.global_position = get_parent().global_position + offset
 	projectile.velocity = direction * projectile_speed
 	projectile.damage = projectile_damage
-	
+
 	var current_map = Globals.current_room
 	current_map.add_child(projectile)
