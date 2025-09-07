e34db3906e25226f25423b4e99a9f035230986e4
Inari Tommiska <inari@jakojaannos.fi>
2025-09-08 01:51:26 +0300

    more dangerous, less annoying pufferfish
    The pufferfish now try to select the target positions from near the
    player, instead of picking anywhere on the navmesh.
    
    This has some advantages:
    1. less chasing for the player, the enemies will come to you. No more
       running around the level, chasing that one pufferfish that keeps on
       camping inside a wall etc.
    2. the pufferfish are now actually dangerous if left unchecked. because
       they follow the player around, they are now significantly more likely
       to be in a position where the spikes hit the player
    
    Did a quick test run with these changes and the puffies feel a lot nicer
    now; not at all as annoying as before!
    
    Did a quick test run with these changes and the puffies feel a lot nicer
    now; not at all as annoying as before!

--- fish/spiky_puffer_fish/spike_fish.gd
+++ fish/spiky_puffer_fish/spike_fish.gd
@@ -2,7 +2,7 @@ class_name SpikeFish
 extends Area2D
 
 const spike_scene = preload("res://fish/spiky_puffer_fish/spike.tscn")
-@export var spike_color:Color = Color(1,1,1,1)
+@export var spike_color: Color = Color(1, 1, 1, 1)
 @export var spike_damage = 1
 @export var min_spike_speed = 100.0
 @export var max_spike_speed = 200.0
@@ -38,25 +38,25 @@ func _ready() -> void:
 func _create_movement_tweens(force_recreate: bool = false):
 	var is_tween1_ok = movement_tween_1 != null && movement_tween_1.is_running()
 	var is_tween2_ok = movement_tween_2 != null && movement_tween_2.is_running()
-	
+
 	if !is_tween1_ok || force_recreate:
 		if movement_tween_1 != null:
 			movement_tween_1.kill()
-		
+
 		var end = pick_random_target_position()
 		var time = randf_range(1.0, 2.0)
-		
+
 		movement_tween_1 = create_tween()
 		movement_tween_1.tween_property(self, "target1", end, time)
 		movement_tween_1.tween_callback(_create_movement_tweens)
-	
+
 	if !is_tween2_ok || force_recreate:
 		if movement_tween_2 != null:
 			movement_tween_2.kill()
 
 		var end = pick_random_target_position()
 		var time = randf_range(1.0, 2.0)
-		
+
 		movement_tween_2 = create_tween()
 		movement_tween_2.tween_property(self, "target2", end, time)
 		movement_tween_2.tween_callback(_create_movement_tweens)
@@ -65,7 +65,7 @@ var last_direction: Vector2 = Vector2.RIGHT
 func _physics_process(delta: float) -> void:
 	var target_position = (target1 + target2) / 2.0
 	var previous_position = position
-	
+
 	var d2 = position.distance_squared_to(target_position)
 	var close_enough = 500.0
 	if d2 <= close_enough:
@@ -92,13 +92,13 @@ func attack():
 	ready_to_attack = false
 	var tween = create_tween()
 	tween.tween_property(self, "move_speed", attack_move_speed, 0.5)
-	
+
 	$AttackAnimationPlayer.play("attack")
 	await $AttackAnimationPlayer.animation_finished
-	
+
 	tween.kill()
 	move_speed = normal_move_speed
-	
+
 	start_attack_cooldown(min_attack_cooldown, max_attack_cooldown)
 
 func start_attack_cooldown(min_cd: float, max_cd: float):
@@ -112,16 +112,23 @@ func _shoot_spikes() -> void:
 
 	var spike_count = randi_range(min_spike_count, max_spike_count)
 
-	for _s in spike_count:
-		var spawn = point_on_unit_circle()
-		var velocity = spawn * randf_range(min_spike_speed, max_spike_speed)
-		
+	var angle_per_spike = (2 * PI) / spike_count
+	var max_angle_variation = angle_per_spike / 2.5
+
+	var start_angle = randf() * 2 * PI
+	for i in spike_count:
+		var angle = start_angle + angle_per_spike * i
+		var variation = randf() * max_angle_variation
+
+		var direction = Vector2.from_angle(angle + variation)
+		var velocity = direction * randf_range(min_spike_speed, max_spike_speed)
+
 		var spike: Projectile = spike_scene.instantiate()
-		spike.modulate = spike_color 
+		spike.modulate = spike_color
 		spike.velocity = velocity
-		spike.global_position = $SpikeOrigin.global_position + spawn
+		spike.global_position = $SpikeOrigin.global_position + direction
 		spike.damage = spike_damage
-		
+
 		Globals.current_room.add_child(spike)
 
 
@@ -129,8 +136,25 @@ func pick_random_target_position() -> Vector2:
 	var current_room = Globals.current_room
 	if current_room == null:
 		return position
-	
-	return current_room.get_random_fish_nav_point()
+
+	var player = Globals.player
+	if player == null:
+		return current_room.get_random_fish_nav_point()
+
+	var tries = 0
+	while (tries < 50):
+		tries += 1
+
+		var dx = 4 * 32
+		var dy = 3 * 32
+		var x = randf_range(-dx, dx)
+		var y = randf_range(-8, dy - 8)
+
+		var point = player.global_position + Vector2(y, x)
+		if current_room.is_in_navigable_region(point):
+			return point
+
+	return position
 
 func point_on_unit_circle() -> Vector2:
 	var angle = randf() * 2.0 * PI
