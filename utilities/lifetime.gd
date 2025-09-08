e515ab74d0ce0cfd1bfbbb175353d4cf34dc4f27
Inari Tommiska <inari@jakojaannos.fi>
2025-09-09 00:58:30 +0300

    fix: don't process lifetimes while game is paused

--- utilities/lifetime.gd
+++ utilities/lifetime.gd
@@ -4,7 +4,7 @@ class_name Lifetime
 @export var duration = 3.0
 
 func _ready() -> void:
-	get_tree().create_timer(duration).timeout.connect(_on_timeout)
+	get_tree().create_timer(duration, false).timeout.connect(_on_timeout)
 
 func _on_timeout() -> void:
 	get_parent().queue_free()
